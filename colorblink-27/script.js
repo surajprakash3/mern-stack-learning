aae49d719f87a4a45ac5f10092e752840e1075e8
Suraj Prakash <surajprak201@gmail.com>
2026-01-27 13:12:56 +0530

    boxmoving commit

---
+++ colorblink-27/script.js
@@ -0,0 +1,33 @@
+function colorbox1(){
+let boxa= document.getElementById('a');
+boxa.style.marginLeft="200px";
+boxa.style.background="blue" ;
+let boxb=document.getElementById('b');
+boxb.style.marginTop="-100px";
+boxb.style.background="brown";
+let boxc=document.getElementById('c');
+boxc.style.marginLeft="200px";
+boxc.style.background="orange";
+let boxd=document.getElementById('d');
+boxd.style.marginTop="-100px";
+boxd.style.background="grey";
+}
+function colorbox2(){
+let boxa= document.getElementById('a').style.background="grey";
+let boxb=document.getElementById('b').style.background="blue";
+let boxc=document.getElementById('c').style.background="brown";
+let boxd=document.getElementById('d').style.background="orange";
+}
+function colorbox3(){
+let boxa= document.getElementById('a').style.background="orange";
+let boxb=document.getElementById('b').style.background="grey";
+let boxc=document.getElementById('c').style.background="blue";
+let boxd=document.getElementById('d').style.background="brown";
+}
+function colorbox4(){
+let boxa= document.getElementById('a').style.background="brown";
+let boxb=document.getElementById('b').style.background="orange";
+let boxc=document.getElementById('c').style.background="grey";
+let boxd=document.getElementById('d').style.background="blue";
+}
+
